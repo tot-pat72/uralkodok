b180850355b49b7f932b9aa27018973413ad91ee
Patrik <patsboy06@gmail.com>
2024-12-06 18:35:55 +0100

    linearis utasitas vegrehajtas

--- main.js
+++ main.js
@@ -1,133 +1,133 @@
-const table = document.createElement('table');
-document.body.appendChild(table);
+const table = document.createElement('table'); //Táblázat lértehozása.
+document.body.appendChild(table); //Táblázat hozzáadása a dokumentumhoz.
 
-const colgroup = document.createElement('colgroup');
-table.appendChild(colgroup);
+const colgroup = document.createElement('colgroup'); //Colgroup létrehozása.
+table.appendChild(colgroup); //Colgroup hozzáadása a táblázathoz.
 
-const colSpan_1 = document.createElement('col');
-colSpan_1.className = "columns";
-colgroup.appendChild(colSpan_1);
+const colSpan_1 = document.createElement('col'); //1.oszlop létrehozása.
+colSpan_1.className = "columns"; //className megadása az 1.oszlopnak.
+colgroup.appendChild(colSpan_1); //1.oszlop hozzáadása a Colgouphoz.
 
-const colSpan_2 = document.createElement('col');
-colgroup.appendChild(colSpan_2);
+const colSpan_2 = document.createElement('col'); //2.oszlop létrehozása.
+colgroup.appendChild(colSpan_2); //2.oszlop hozzáadása a Colgouphoz.
 
-const colSpan_3 = document.createElement('col');
-colSpan_3.className = "columns";
-colgroup.appendChild(colSpan_3);
+const colSpan_3 = document.createElement('col'); //3.oszlop létrehozása.
+colSpan_3.className = "columns"; //className megadása a 3.oszlopnak.
+colgroup.appendChild(colSpan_3); //3.oszlop hozzáadása a Colgouphoz.
 
-const thead = document.createElement('thead');
-table.appendChild(thead);
+const thead = document.createElement('thead'); //Fejléc létrehozása.
+table.appendChild(thead); //Fejléc hozzáadása a táblázathoz.
 
-const tr = document.createElement('tr');
-thead.appendChild(tr);
+const tr = document.createElement('tr'); //Fejlécben lévő sor létrehozása.
+thead.appendChild(tr); //Fejlécben lévő sor hozzáadása a fejléchez.
 
-const th_1 = document.createElement('th');
-th_1.innerHTML = "Uralkodó";
-tr.appendChild(th_1);
+const th_1 = document.createElement('th'); //A fejléc sorában lévő 1.cella létrehozása.
+th_1.innerHTML = "Uralkodó"; //A cella tartalmának megadása.
+tr.appendChild(th_1); //A fejléc sorában lévő 1.cella hozzáadása a fejlécben lévő sorhoz.
 
-const th_2 = document.createElement('th');
-th_2.innerHTML = "Esemény";
-tr.appendChild(th_2);
+const th_2 = document.createElement('th'); //A fejléc sorában lévő 2.cella létrehozása.
+th_2.innerHTML = "Esemény"; //A cella tartalmának megadása.
+tr.appendChild(th_2); //A fejléc sorában lévő 2.cella hozzáadása a fejlécben lévő sorhoz.
 
-const th_3 = document.createElement('th');
-th_3.innerHTML = "Évszám";
-tr.appendChild(th_3);
+const th_3 = document.createElement('th'); //A fejléc sorában lévő 3.cella létrehozása.
+th_3.innerHTML = "Évszám"; //A cella tartalmának megadása.
+tr.appendChild(th_3); //A fejléc sorában lévő 3.cella hozzáadása a fejlécben lévő sorhoz.
 
-const tbody = document.createElement('tbody');
-table.appendChild(tbody);
+const tbody = document.createElement('tbody'); //Törzs létrehozása.
+table.appendChild(tbody); //Törzs hozzáadása a táblázathoz.
 
-const tr_1 = document.createElement('tr');
-tbody.appendChild(tr_1);
+const tr_1 = document.createElement('tr'); //Törzsben lévő 1.sor létrehozása.
+tbody.appendChild(tr_1); //Törzsben lévő 1.sor hozzáadása a törzshöz.
 
-const td_1_1 = document.createElement('td');
-td_1_1.innerHTML = "I.István";
-tr_1.appendChild(td_1_1);
-td_1_1.rowSpan = 2;
+const td_1_1 = document.createElement('td'); //A törzs 1.sorában lévő 1.cella létrehozása.
+td_1_1.innerHTML = "I.István"; //A cella tartalmának megadása.
+tr_1.appendChild(td_1_1); //A törzs 1.sorában lévő 1.cella hozzáadása a törzsben lévő 1.sorhoz.
+td_1_1.rowSpan = 2; //A törzs 1.sorában lévő 1.cellának megadjuk, hogy 2 sort csatoljon össze.
 
-const td_1_2 = document.createElement('td');
-td_1_2.innerHTML = "Koronázás";
-tr_1.appendChild(td_1_2);
+const td_1_2 = document.createElement('td'); //A törzs 1.sorában lévő 2.cella létrehozása.
+td_1_2.innerHTML = "Koronázás"; //A cella tartalmának megadása.
+tr_1.appendChild(td_1_2); //A törzs 1.sorában lévő 2.cella hozzáadása a törzsben lévő 1.sorhoz.
 
-const td_1_3 = document.createElement('td');
-td_1_3.innerHTML = "1000";
-tr_1.appendChild(td_1_3);
+const td_1_3 = document.createElement('td'); //A törzs 1.sorában lévő 3.cella létrehozása.
+td_1_3.innerHTML = "1000"; //A cella tartalmának megadása.
+tr_1.appendChild(td_1_3); //A törzs 1.sorában lévő 3.cella hozzáadása a törzsben lévő 1.sorhoz.
 
-const tr_2 = document.createElement('tr');
-tbody.appendChild(tr_2);
+const tr_2 = document.createElement('tr'); //Törzsben lévő 2.sor létrehozása.
+tbody.appendChild(tr_2); //Törzsben lévő 2.sor hozzáadása a törzshöz.
 
-const td_2_1 = document.createElement('td');
-td_2_1.innerHTML = "Pannonhalmi apátság megalapítása";
-tr_2.appendChild(td_2_1);
+const td_2_1 = document.createElement('td'); //A törzs 2.sorában lévő 1.cella létrehozása.
+td_2_1.innerHTML = "Pannonhalmi apátság megalapítása"; //A cella tartalmának megadása.
+tr_2.appendChild(td_2_1); //A törzs 2.sorában lévő 1.cella hozzáadása a törzsben lévő 2.sorhoz.
 
-const td_2_2 = document.createElement('td');
-td_2_2.innerHTML = "1001";
-tr_2.appendChild(td_2_2);
+const td_2_2 = document.createElement('td'); //A törzs 2.sorában lévő 2.cella létrehozása.
+td_2_2.innerHTML = "1001"; //A cella tartalmának megadása.
+tr_2.appendChild(td_2_2); //A törzs 2.sorában lévő 2.cella hozzáadása a törzsben lévő 2.sorhoz.
 
-const tr_3 = document.createElement('tr');
-tbody.appendChild(tr_3);
+const tr_3 = document.createElement('tr'); //Törzsben lévő 3.sor létrehozása.
+tbody.appendChild(tr_3); //Törzsben lévő 3.sor hozzáadása a törzshöz.
 
-const td_3_1 = document.createElement('td');
-td_3_1.innerHTML = "IV. Béla";
-tr_3.appendChild(td_3_1);
+const td_3_1 = document.createElement('td'); //A törzs 3.sorában lévő 1.cella létrehozása.
+td_3_1.innerHTML = "IV. Béla"; //A cella tartalmának megadása.
+tr_3.appendChild(td_3_1); //A törzs 3.sorában lévő 1.cella hozzáadása a törzsben lévő 3.sorhoz.
 
-const td_3_2 = document.createElement('td');
-td_3_2.innerHTML = "tatárjárás";
-tr_3.appendChild(td_3_2);
+const td_3_2 = document.createElement('td'); //A törzs 3.sorában lévő 2.cella létrehozása.
+td_3_2.innerHTML = "tatárjárás"; //A cella tartalmának megadása.
+tr_3.appendChild(td_3_2); //A törzs 3.sorában lévő 2.cella hozzáadása a törzsben lévő 3.sorhoz.
 
-const td_3_3 = document.createElement('td');
-td_3_3.innerHTML = "1241-1242";
-tr_3.appendChild(td_3_3);
+const td_3_3 = document.createElement('td'); //A törzs 3.sorában lévő 3.cella létrehozása.
+td_3_3.innerHTML = "1241-1242"; //A cella tartalmának megadása.
+tr_3.appendChild(td_3_3); //A törzs 3.sorában lévő 3.cella hozzáadása a törzsben lévő 3.sorhoz.
 
-const tr_4 = document.createElement('tr');
-tbody.appendChild(tr_4);
+const tr_4 = document.createElement('tr'); //Törzsben lévő 4.sor létrehozása.
+tbody.appendChild(tr_4); //Törzsben lévő 4.sor hozzáadása a törzshöz.
 
-const td_4_1 = document.createElement('td');
-td_4_1.innerHTML = "Mátyás Király";
-tr_4.appendChild(td_4_1);
-td_4_1.rowSpan = 2;
+const td_4_1 = document.createElement('td'); //A törzs 4.sorában lévő 1.cella létrehozása.
+td_4_1.innerHTML = "Mátyás Király"; //A cella tartalmának megadása.
+tr_4.appendChild(td_4_1); //A törzs 4.sorában lévő 1.cella hozzáadása a törzsben lévő 4.sorhoz.
+td_4_1.rowSpan = 2; //A törzs 4.sorában lévő 1.cellának megadjuk, hogy 2 sort csatoljon össze.
 
-const td_4_2 = document.createElement('td');
-td_4_2.innerHTML = "Bécs elfoglalása";
-tr_4.appendChild(td_4_2);
+const td_4_2 = document.createElement('td'); //A törzs 4.sorában lévő 2.cella létrehozása.
+td_4_2.innerHTML = "Bécs elfoglalása"; //A cella tartalmának megadása.
+tr_4.appendChild(td_4_2); //A törzs 4.sorában lévő 2.cella hozzáadása a törzsben lévő 4.sorhoz.
 
-const td_4_3 = document.createElement('td');
-td_4_3.innerHTML = "1485";
-tr_4.appendChild(td_4_3);
+const td_4_3 = document.createElement('td'); //A törzs 4.sorában lévő 3.cella létrehozása.
+td_4_3.innerHTML = "1485"; //A cella tartalmának megadása.
+tr_4.appendChild(td_4_3); //A törzs 4.sorában lévő 3.cella hozzáadása a törzsben lévő 4.sorhoz.
 
-const tr_5 = document.createElement('tr');
-tbody.appendChild(tr_5);
+const tr_5 = document.createElement('tr'); //Törzsben lévő 5.sor létrehozása.
+tbody.appendChild(tr_5); //Törzsben lévő 5.sor hozzáadása a törzshöz.
 
-const td_5_1 = document.createElement('td');
-td_5_1.innerHTML = "Kenyérmezei csata";
-tr_5.appendChild(td_5_1);
+const td_5_1 = document.createElement('td'); //A törzs 5.sorában lévő 1.cella létrehozása.
+td_5_1.innerHTML = "Kenyérmezei csata"; //A cella tartalmának megadása.
+tr_5.appendChild(td_5_1); //A törzs 5.sorában lévő 1.cella hozzáadása a törzsben lévő 5.sorhoz.
 
-const td_5_2 = document.createElement('td');
-td_5_2.innerHTML = "1479";
-tr_5.appendChild(td_5_2);
+const td_5_2 = document.createElement('td'); //A törzs 5.sorában lévő 2.cella létrehozása.
+td_5_2.innerHTML = "1479"; //A cella tartalmának megadása.
+tr_5.appendChild(td_5_2); //A törzs 5.sorában lévő 2.cella hozzáadása a törzsben lévő 5.sorhoz.
 
-const tr_6 = document.createElement('tr');
-tbody.appendChild(tr_6);
+const tr_6 = document.createElement('tr'); //Törzsben lévő 6.sor létrehozása.
+tbody.appendChild(tr_6); //Törzsben lévő 6.sor hozzáadása a törzshöz.
 
-const td_6_1 = document.createElement('td');
-td_6_1.innerHTML = "II. Rákóczi Ferenc";
-tr_6.appendChild(td_6_1);
-td_6_1.rowSpan = 2;
+const td_6_1 = document.createElement('td'); //A törzs 6.sorában lévő 1.cella létrehozása.
+td_6_1.innerHTML = "II. Rákóczi Ferenc"; //A cella tartalmának megadása.
+tr_6.appendChild(td_6_1); //A törzs 6.sorában lévő 1.cella hozzáadása a törzsben lévő 6.sorhoz.
+td_6_1.rowSpan = 2; //A törzs 6.sorában lévő 1.cellának megadjuk, hogy 2 sort csatoljon össze.
 
-const td_6_2 = document.createElement('td');
-td_6_2.innerHTML = "A szabadságharc kezdete";
-tr_6.appendChild(td_6_2);
+const td_6_2 = document.createElement('td'); //A törzs 6.sorában lévő 2.cella létrehozása.
+td_6_2.innerHTML = "A szabadságharc kezdete"; //A cella tartalmának megadása.
+tr_6.appendChild(td_6_2); //A törzs 6.sorában lévő 2.cella hozzáadása a törzsben lévő 6.sorhoz.
 
-const td_6_3 = document.createElement('td');
-td_6_3.innerHTML = "1703";
-tr_6.appendChild(td_6_3);
+const td_6_3 = document.createElement('td'); //A törzs 6.sorában lévő 3.cella létrehozása.
+td_6_3.innerHTML = "1703"; //A cella tartalmának megadása.
+tr_6.appendChild(td_6_3); //A törzs 6.sorában lévő 3.cella hozzáadása a törzsben lévő 6.sorhoz.
 
-const tr_7 = document.createElement('tr');
-tbody.appendChild(tr_7);
+const tr_7 = document.createElement('tr'); //Törzsben lévő 7.sor létrehozása.
+tbody.appendChild(tr_7); //Törzsben lévő 7.sor hozzáadása a törzshöz.
 
-const td_7_1 = document.createElement('td');
-td_7_1.innerHTML = "A szabadságharc vége";
-tr_7.appendChild(td_7_1);
+const td_7_1 = document.createElement('td'); //A törzs 7.sorában lévő 1.cella létrehozása.
+td_7_1.innerHTML = "A szabadságharc vége"; //A cella tartalmának megadása.
+tr_7.appendChild(td_7_1); //A törzs 7.sorában lévő 1.cella hozzáadása a törzsben lévő 7.sorhoz.
 
-const td_7_2 = document.createElement('td');
-td_7_2.innerHTML = "1711";
-tr_7.appendChild(td_7_2);
+const td_7_2 = document.createElement('td'); //A törzs 7.sorában lévő 2.cella létrehozása.
+td_7_2.innerHTML = "1711"; //A cella tartalmának megadása.
+tr_7.appendChild(td_7_2); //A törzs 7.sorában lévő 2.cella hozzáadása a törzsben lévő 7.sorhoz.
