2d4aaa3d6f4d3efc53ca02b9ec4672e05136d71b
Patrik <patsboy06@gmail.com>
2024-12-19 16:03:46 +0100

    form generalas fuggveny bevezetese

--- main.js
+++ main.js
@@ -56,6 +56,127 @@ colgroup.appendChild(colSpan_3); //3.oszlop hozzáadása a Colgouphoz.
 const thead = document.createElement('thead'); //Fejléc létrehozása.
 table.appendChild(thead); //Fejléc hozzáadása a táblázathoz.
 
+const tbody = document.createElement('tbody'); //Törzs létrehozása.
+table.appendChild(tbody); //Törzs hozzáadása a táblázathoz.
+
+function generateForm(){ //A generateForm függvény meghatározása.
+    const form = document.createElement('form'); //Form létrehozása.
+    form.id = 'form'; //A form id-jének megadása.
+    document.body.appendChild(form); //A form hozzáadása a dokumentum törzséhez.
+
+
+    const div_1 = document.createElement('div'); //A form 1.sorának a létrehozása.
+    div_1.classList.add('field'); //A field class hozzáadása a div_1-hez.
+
+    const label_1 = document.createElement('label'); //A label_1 elem létrehozása.
+    label_1.htmlFor = 'uralkodo_nev'; //A label_1 for-ának megadása.
+    label_1.innerText = 'Uralkodó neve:'; //A label_1 tartalma az Uralkodó neve: lesz.
+    div_1.appendChild(label_1); //A label_1 hozzáadása a div_1-hez.
+
+    const input_1 = document.createElement('input'); //Az input_1 mező létrehozása.
+    input_1.type = 'text'; //Az input_1 típusának megadása.
+    input_1.id = 'uralkodo_nev'; //Az input_1 id-jének megadása.
+    input_1.name = 'uralkodo_nev'; //Az input_1 nevének megadása.
+    div_1.appendChild(input_1); //Az input_1 hozzáadása a div_1-hez.
+
+    const div_1_error = document.createElement('div'); //A div_1_error div létrehozása.
+    div_1_error.classList = 'error'; //A div_1_error-hoz az error class hozzárendelése.
+    div_1.appendChild(div_1_error); //A div_1_error hozzáadása a div_1-hez.
+
+    form.appendChild(div_1); //A div_1 hozzáadása a formhoz.
+    
+
+    const div_2 = document.createElement('div'); //A form 2.sorának a létrehozása.
+    div_2.classList.add('field'); //A field class hozzáadása a div_2-höz.
+
+    const label_2 = document.createElement('label'); //A label_2 elem létrehozása.
+    label_2.htmlFor = 'esemeny1'; //A label_2 for-ának megadása.
+    label_2.innerText = 'Első esemény:'; //A label_2 tartalma az Első esemény: lesz.
+    div_2.appendChild(label_2); //A label_2 hozzáadása a div_2-höz.
+
+    const input_2 = document.createElement('input'); //Az input_2 mező létrehozása.
+    input_2.type = 'text'; //Az input_2 típusának megadása.
+    input_2.id = 'esemeny1'; //Az input_2 id-jének megadása.
+    input_2.name = 'esemeny1'; //Az input_2 nevének megadása.
+    div_2.appendChild(input_2); //Az input_2 hozzáadása a div_2-höz.
+
+    const div_2_error = document.createElement('div'); //A div_2_error div létrehozása.
+    div_2_error.classList = 'error'; //A div_2_error-hoz az error class hozzárendelése.
+    div_2.appendChild(div_2_error); //A div_2_error hozzáadása a div_2-höz.
+
+    form.appendChild(div_2); //A div_2 hozzáadása a formhoz.
+
+
+    const div_3 = document.createElement('div'); //A form 3.sorának a létrehozása.
+    div_3.classList.add('field'); //A field class hozzáadása a div_3-hoz.
+
+    const label_3 = document.createElement('label'); //A label_3 elem létrehozása.
+    label_3.htmlFor = 'evszam1'; //A label_3 for-ának megadása.
+    label_3.innerText = 'Első esemény évszáma:'; //A label_3 tartalma az Első esemény évszáma: lesz.
+    div_3.appendChild(label_3); //A label_3 hozzáadása a div_3-hoz.
+
+    const input_3 = document.createElement('input'); //Az input_3 mező létrehozása.
+    input_3.type = 'text'; //Az input_3 típusának megadása.
+    input_3.id = 'evszam1'; //Az input_3 id-jének megadása.
+    input_3.name = 'evszam1'; //Az input_3 nevének megadása.
+    div_3.appendChild(input_3); //Az input_3 hozzáadása a div_3-hoz.
+
+    const div_3_error = document.createElement('div'); //A div_3_error div létrehozása.
+    div_3_error.classList = 'error'; //A div_3_error-hoz az error class hozzárendelése.
+    div_3.appendChild(div_3_error); //A div_3_error hozzáadása a div_3-hoz.
+
+    form.appendChild(div_3); //A div_3 hozzáadása a formhoz.
+
+
+    const div_4 = document.createElement('div'); //A form 4.sorának a létrehozása.
+    div_4.classList.add('field'); //A field class hozzáadása a div_4-hez.
+
+    const label_4 = document.createElement('label'); //A label_4 elem létrehozása.
+    label_4.htmlFor = 'esemeny2'; //A label_4 for-ának megadása.
+    label_4.innerText = 'Második esemény:'; //A label_4 tartalma a Második esemény: lesz.
+    div_4.appendChild(label_4); //A label_4 hozzáadása a div_4-hez.
+
+    const input_4 = document.createElement('input'); //Az input_4 mező létrehozása.
+    input_4.type = 'text'; //Az input_4 típusának megadása.
+    input_4.id = 'esemeny2'; //Az input_4 id-jének megadása.
+    input_4.name = 'esemeny2'; //Az input_4 nevének megadása.
+    div_4.appendChild(input_4); //Az input_4 hozzáadása a div_4-hez.
+
+    const div_4_error = document.createElement('div'); //A div_4_error div létrehozása.
+    div_4_error.classList = 'error'; //A div_4_error-hoz az error class hozzárendelése.
+    div_4.appendChild(div_4_error); //A div_4_error hozzáadása a div_4-hez.
+
+    form.appendChild(div_4); //A div_4 hozzáadása a formhoz.
+
+
+    const div_5 = document.createElement('div'); //A form 5.sorának a létrehozása.
+    div_5.classList.add('field'); //A field class hozzáadása a div_5-höz.
+
+    const label_5 = document.createElement('label'); //A label_5 elem létrehozása.
+    label_5.htmlFor = 'evszam2'; //A label_5 for-ának megadása.
+    label_5.innerText = 'Második esemény évszáma'; //A label_5 tartalma a Második esemény évszáma: lesz.
+    div_5.appendChild(label_5); //A label_5 hozzáadása a div_5-höz.
+
+    const input_5 = document.createElement('input'); //Az input_5 mező létrehozása.
+    input_5.type = 'text'; //Az input_5 típusának megadása.
+    input_5.id = 'evszam2'; //Az input_5 id-jének megadása.
+    input_5.name = 'evszam2'; //Az input_5 nevének megadása.
+    div_5.appendChild(input_5); //Az input_5 hozzáadása a div_5-höz.
+
+    const div_5_error = document.createElement('div'); //A div_5_error div létrehozása.
+    div_5_error.classList = 'error'; //A div_5_error-hoz az error class hozzárendelése.
+    div_5.appendChild(div_5_error); //A div_5_error hozzáadása a div_5-höz.
+
+    form.appendChild(div_5); //A div_5 hozzáadása a formhoz.
+
+
+    const button = document.createElement('button'); //A gomb elem létrehozása.
+    button.type = 'submit'; //A gomb típusa submit lesz.
+    button.innerText = 'Hozzáadás'; //A gomb tartalma a Hozzáadás lesz.
+    form.appendChild(button); //A gomb hozzáadása a formhoz.
+}
+generateForm(); //A generateForm függvény meghívása.
+
 function generateHeader(){ //A generateHeader függvény meghatározása.
     const header = ["Uralkodó", "Esemény", "Évszám"]; //A header változóba, a tömb tartalmának az eltárolása.
     const tr = document.createElement('tr'); //Fejlécben lévő sor létrehozása.
@@ -66,9 +187,6 @@ function generateHeader(){ //A generateHeader függvény meghatározása.
     }
 }
 generateHeader(); //A generateHeader függvény meghívása.
-
-const tbody = document.createElement('tbody'); //Törzs létrehozása.
-table.appendChild(tbody); //Törzs hozzáadása a táblázathoz.
 
 function render(array){ //A render függvény meghatározása, amelynek a bemeneti paramétere az array lesz.
     for(const currentElement of array) { //Végighaladás az arrayen, a currentElement az aktuális elem.
